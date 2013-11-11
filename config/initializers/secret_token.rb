f9b088624d70db728d8d26c970e7eb7913c4492e
en <kmagnusson63@academic.rrc.ca>
2013-11-10 18:34:51 -0600

    First commit of mono twitter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonoTwitter::Application.config.secret_token = 'eb3e6461aa9ef1f9d1e9b852530c284bc815ce6eaf056cf6380d56b64458d883925193f7b0de7c76a2a0f822fdac24d6be512ba8aecc323c99e278640bc01e66'
